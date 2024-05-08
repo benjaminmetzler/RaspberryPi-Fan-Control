29f0f6162200eab437aad49232f006fbdd22decc
Benjamin Metzler <4074316+benjaminmetzler@users.noreply.github.com>
2024-05-07 20:11:50 -0700

    Add grace period for turning off fan
    The original script just turned the fan on or off
    depending on if the current temp was above some
    threshold. Around that threshold, the fan could turn
    on and off repeatedly as the temp rose slightly above
    and below.
    
    This new code adds a 10 minute grace period such that
    after the fan turns on, the code will wait 10 minutes
    before turning off the fan, regardless of if the temp
    is below the established threshold.

--- temp-fan.sh
+++ temp-fan.sh
@@ -1,16 +1,57 @@
 #!/bin/bash
+
 ontemp=48
-gpio -g mode 3 out
-temp=$(vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')
-echo $temp #use BCM pin 3, as pin 2 is used for PSU control
+buffer_time=600  # 10 minutes buffer time in seconds
+fan_state_file="/tmp/fan_state.txt"
+gpio_pin=14
+
+gpio -g mode $gpio_pin out
+
+# Function to check if the fan has been on for at least the buffer time
+in_grace_time() {
+    if [ ! -f "$fan_state_file" ]; then
+        echo "Fan state file does not exist"
+        return 1
+    fi
+    last_on_time=$(cat "$fan_state_file")
+    current_time=$(date +%s)
+    time_diff=$((current_time - last_on_time))
+    echo "Last on time: $last_on_time"
+    echo "Current time: $current_time"
+    echo "Buffer: $buffer_time"
+    echo "Time difference: $time_diff"
+    if [ $time_diff -ge $buffer_time ]; then
+        echo "Not in grace time, returning false"
+        return 1
+    else
+        echo "In grace time, returning true"
+        return 0
+    fi
+}
+
+
+# Function to update the fan state file
+update_fan_state() {
+    echo $(date +%s) > "$fan_state_file"
+}
+
+temp=$(vcgencmd measure_temp | grep -E -o '[0-9]*\.[0-9]*')
 temp0=${temp%.*}
+
+echo $temp
 echo $temp0
 
-if [ $temp0 -gt $ontemp ] 
-then
-	echo greater than $ontemp fan on
-	gpio -g write 3 1
+if [ $temp0 -gt $ontemp ]; then
+    echo "Greater than $ontemp, fan on"
+    gpio -g write $gpio_pin 1
+    update_fan_state
+elif in_grace_time; then
+    echo "Fan is staying on due to ${buffer_time} second buffer time"
+    gpio -g write $gpio_pin 1
 else
-	echo less than or equal to  $ontemp fan off
-	gpio -g write 3 0
+    echo "Less than or equal to $ontemp, fan off"
+    gpio -g write $gpio_pin 0
+    if [ -f "$fan_state_file" ]; then
+        rm "$fan_state_file"
+    fi
 fi
